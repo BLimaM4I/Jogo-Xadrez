76a53ccfe230749fd05ccdfcdd51362a35650c22
BLimaM4I <geral@m4i.pt>
2020-11-02 17:42:40 +0000

    Start to create the board piece class

--- src/boardGame/Position.java
+++ src/boardGame/Position.java
@@ -5,7 +5,6 @@ public class Position {
     private int row;
     private int column;
 
-
     public Position(int row, int column) {
         this.row = row;
         this.column = column;
@@ -28,7 +27,7 @@ public class Position {
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         return row + ", " + column;
     }
 
