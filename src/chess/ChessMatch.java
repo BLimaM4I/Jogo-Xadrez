283365efafbd4a758bd96da00f1d5590607f8906
BLimaM4I <geral@m4i.pt>
2020-11-03 11:38:42 +0000

    Defensive programming

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -13,20 +13,20 @@ public class ChessMatch {
         initialSetup();
     }
 
-    public ChessPiece[][] getPieces(){
+    public ChessPiece[][] getPieces() {
         ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i=0; i<board.getRows(); i++){
-            for (int j=0; j<board.getColumns(); j++){
-                mat[i][j] = (ChessPiece) board.piece(i,j);
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getColumns(); j++) {
+                mat[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
         return mat;
     }
 
-    private void initialSetup(){
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece(new King(board, Color.WHITE), new Position(0,0));
-        board.placePiece(new King(board, Color.BLACK), new Position(7,7));
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(1, 1));
+        board.placePiece(new King(board, Color.WHITE), new Position(0, 0));
+        board.placePiece(new King(board, Color.BLACK), new Position(2, 2));
 
     }
 }
